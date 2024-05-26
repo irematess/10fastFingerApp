98c376063bb17cb81e7950091da5692336009d1d
irem ates <irematees16@gmail.com>
2024-05-26 14:21:10 +0300

    Add ui

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <script setup></script>
 
 <template>
-  <div></div>
+  <header class="bg-[#190019] py-8 px-4 text-3xl text-white font-bold">
+    <h1>10 FAST FINGER APP</h1>
+  </header>
+  <div class="container mx-auto py-36 flex flex-col gap-4">
+    <div class="flex flex-row items-center justify-end gap-4 text-xl">
+      <i class="fa-solid fa-retweet p-3 rounded-md bg-white text-[#d03872] replay"></i>
+      <span class="bg-[#d03872] p-2 rounded-md text-white time" >1:00</span>
+    </div>
+    <div class="bg-white p-4 text-2xl font-serif tracking-wide leading-10 rounded-md words">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti, quasi? Quam, repellat! Asperiores tempora consectetur numquam autem, accusamus ad tenetur eveniet animi blanditiis odit quae minus excepturi optio ut sit voluptatibus dolorum sapiente harum quibusdam natus exercitationem minima illo nihil? Labore aliquid at asperiores eius cumque dolorum doloribus deserunt vero.
+    </div>
+    <div class="flex justify-center">
+      <input type="text" class="p-3 px-4 w-3/4 rounded-md" placeholder="text here">
+    </div>
+  </div>
 </template>
+
